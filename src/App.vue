05ec320f39fb2025c5d36b02c41e48c0732d70b1
danieledisco <daniele.disco.1965@gmail.com>
2024-02-18 22:32:54 +0100

    Step 2 - Partial First Task

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,37 @@ export default {
     return {
       state
     }
+
+  },
+  methods: {
+    makeFlags(input) {
+      let out;
+      if (input.original_language === 'en')
+        out = 'fi fi-gb';
+      else if (input.original_language === 'ja')
+        out = 'fi fi-jp';
+      else
+        out = 'fi fi-' + input.original_language;
+      return out;
+    }
   }
 }
+
 </script>
  
 <template>
   <input name="searchText" type="text" placeholder="Name movie or series here" v-model="state.stringSearch">
   <button @click="state.getMovie">Filter</button>
-  <ul v-for="movie in state.out">
+  <ul v-for="movie in    state.out   ">
     <li>Title : {{ movie.title }}</li>
     <li>Original title : {{ movie.original_title }}</li>
-    <li>Language: {{ movie.original_language }}</li>
+    <li>Language : {{ movie.original_language }}</li>
+    <li><span v-bind:class="makeFlags(movie)"></span></li>
     <li>Vote: {{ movie.vote_count }}</li>
     <li>Vote averege: {{ movie.vote_average }}</li>
   </ul>
 </template>
  
 <style scoped></style>
+
+
